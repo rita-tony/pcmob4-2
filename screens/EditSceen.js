5bed13ba7182cc6db58c622fbe27e0f5a165e68a
rita_tony <rita_tony@hotmail.com>
2022-01-18 16:03:09 +0800

    Code Clean up and Added TouchableWithoutFeedback

--- screens/EditSceen.js
+++ screens/EditSceen.js
@@ -1,96 +1,98 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Keyboard, TouchableWithoutFeedback } from 'react-native';
 
-export default function EditScreen ({ navigation, route}) {
-    const idNote = route.params.id;
-    const [titleNote, setTitleNote] = useState(route.params.title);
-    const [detailsNote, setDetailsNote] = useState(route.params.details);
+export default function EditScreen({ navigation, route }) {
+  const idNote = route.params.id;
+  const [titleNote, setTitleNote] = useState(route.params.title);
+  const [detailsNote, setDetailsNote] = useState(route.params.details);
 
-    return (
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-          <Text style={styles.labelStyle}>Edit your todo ID # {idNote}</Text>
-          <TextInput
-            style={styles.textInputStyle}
-            maxLength={40}
-            value={titleNote}
-            onChangeText={(newTitleNote) => setTitleNote(newTitleNote)}
-            >            
-          </TextInput>
+        <Text style={styles.labelStyle}>Edit your todo ID # {idNote}</Text>
+        <TextInput
+          style={styles.textInputStyle}
+          maxLength={40}
+          value={titleNote}
+          onChangeText={(newTitleNote) => setTitleNote(newTitleNote)}
+        >
+        </TextInput>
 
-          <TextInput
-            style={styles.textInputStyle}
-            maxLength={50}
-            value={detailsNote}
-            onChangeText={(newDetailsNote) => setDetailsNote(newDetailsNote)}
-            >            
-          </TextInput>
+        <TextInput
+          style={styles.textInputStyle}
+          maxLength={50}
+          value={detailsNote}
+          onChangeText={(newDetailsNote) => setDetailsNote(newDetailsNote)}
+        >
+        </TextInput>
 
-          <View style={styles.buttonsStyle}>
-              <TouchableOpacity
-                onPress = { () => navigation.navigate("Notes", {titleNote, detailsNote, idNote}) }
-                style={[styles.buttonStyle, styles.submitButtonStyle]}
-              >
-                  <Text style={styles.buttonTextStyle}>Update</Text>
-              </TouchableOpacity>
+        <View style={styles.buttonsStyle}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Notes", { titleNote, detailsNote, idNote })}
+            style={[styles.buttonStyle, styles.submitButtonStyle]}
+          >
+            <Text style={styles.buttonTextStyle}>Update</Text>
+          </TouchableOpacity>
 
-              <TouchableOpacity
-                onPress={ () => navigation.goBack()}
-                style={[styles.buttonStyle, styles.cancelButtonStyle]}
-              >
-                  <Text style={styles.buttonTextStyle}>Cancel</Text>
-              </TouchableOpacity>
-          </View>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={[styles.buttonStyle, styles.cancelButtonStyle]}
+          >
+            <Text style={styles.buttonTextStyle}>Cancel</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    );
-  }
+    </TouchableWithoutFeedback>
+  );
+}
 
-  
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    
-    dismissStyle: {
-      color: "orange",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-    labelStyle:{
-        fontSize: 24,
-        fontWeight: "bold",
-    },
+  dismissStyle: {
+    color: "orange",
+  },
 
-    textInputStyle: {
-        margin: 20,
-        borderWidth: 1,
-        width:'80%',
-        padding: 10,
-        borderColor: '#ccc'
-    },
+  labelStyle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
 
-    buttonsStyle: {
-        flexDirection: 'row',
-    },
+  textInputStyle: {
+    margin: 20,
+    borderWidth: 1,
+    width: '80%',
+    padding: 10,
+    borderColor: '#ccc'
+  },
 
-    buttonStyle: {
-        padding: 10,
-        margin: 10,
-        backgroundColor:'green',
-    },
+  buttonsStyle: {
+    flexDirection: 'row',
+  },
 
-    submitButtonStyle: {
-        backgroundColor: 'orange',
-    },
+  buttonStyle: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: 'green',
+  },
 
-    cancelButtonStyle: {
-        backgroundColor: 'red',
-    },
+  submitButtonStyle: {
+    backgroundColor: 'orange',
+  },
 
-    buttonTextStyle: {
-        fontWeight: 'bold',
-        color: 'white',
-    },
-  
-  });
+  cancelButtonStyle: {
+    backgroundColor: 'red',
+  },
+
+  buttonTextStyle: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+});
